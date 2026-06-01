72512313f8b11fe5743da2e0e35217013bbf157c
unknown <you@example.com>
2026-06-01 20:54:42 +0530

    Configure backend for Vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -79,11 +79,4 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT =
-  process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(
-    `Server running on ${PORT}`
-  );
-});
+module.exports = app;
